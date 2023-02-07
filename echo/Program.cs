67668f5d8f4a82fedb870976b868e0349fd0552d
BuildTools <unconfigured@null.spigotmc.org>
2023-02-07 10:07:01 +0000

    Added methods required to update grid as changes occurr

--- echo/Program.cs
+++ echo/Program.cs
@@ -1,23 +1,21 @@
-﻿using System.Drawing;
-using System.Globalization;
-
-namespace echo;
+﻿namespace echo;
 
 class Tile
 {
     // Tile object for pond grid
     public string type = string.Empty;
-    public string character = string.Empty;
+    public char character = '.';
 
 }
-class Grid{
+class Grid
+{
     // Pond grid containing tiles of water/ pebble/ splash
     public static int size = 50; // Determines the size of the pond
-    public static Tile[,] grid = generateGrid();
+    public Tile[,] grid = generateGrid();
 
     public static Tile[,] generateGrid()
     {
-        Tile[,] generatedGrid = new Tile[size,size];
+        Tile[,] generatedGrid = new Tile[size, size];
         for (int i = 0; i < size; i++)
         {
             for (int j = 0; j < size; j++)
@@ -25,9 +23,9 @@ class Grid{
                 generatedGrid[i, j] = new Tile
                 {
                     type = "water",
-                    character = "."
+                    character = '.'
                 };
-                
+
             }
         }
         return generatedGrid;
@@ -36,21 +34,40 @@ class Grid{
     public void OutputGrid()
     {
         // Prints out the current contents of the pond to the console
-        string top = new string('_', size);
-        string bottom = new string('-', size);
-        Console.Write($"\n  {top}\n");
-        for (int i = 0; i< size; i++)
+        //string top = new string('_', size);
+        //string bottom = new string('-', size);
+        //Console.Write($"\n  {top}\n");
+        for (int i = 0; i < size; i++)
         {
-            Console.Write("| ");
-            for(int j = 0; j < size; j++)
+            //Console.Write("| ");
+            for (int j = 0; j < size; j++)
             {
-                Console.Write(grid[i,j].character);
+                Console.Write(grid[i, j].character);
             }
-            Console.Write(" |\n");
+            Console.Write("\n");
         }
-        Console.Write($"  {bottom}");
+        //Console.Write($"  {bottom}");
     }
-    public static int[] GenerateRandXY()
+
+    public void UpdateGrid()
+    {
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(grid[x, y].character);
+            }
+        }
+    }
+    public void UpdateTileOutput(int[] xy)
+    {
+        int x = xy[0];
+        int y = xy[1];
+        Console.SetCursorPosition(x, y);
+        Console.Write(grid[x, y].character);
+    }
+    public int[] GenerateRandXY()
     {
         // Generate random X Y coordinates
         int[] randXY = new int[2];
@@ -62,38 +79,41 @@ class Grid{
         return randXY;
     }
 
-    public static int changeTile(int[] coord, string type)
+    public void changeTile(int[] coord, string type)
     {
         // Changes tile type using xy coordinates
-        string character = string.Empty;
-        if (type == "water")
+        char character = 'X';
+        if (type.Contains("water"))
         {
-            character = ".";
+            character = '.';
         }
-        if (type == "pebble")
+        if (type.Contains("pebble"))
         {
-            character = "0";
+            character = '0';
         }
-        if (type == "splash")
+        if (type.Contains("splash"))
         {
-            character = "o";
-        }
-        else
-        {
-            return 1;
+            character = 'o';
         }
         grid[coord[0], coord[1]].type = type;
         grid[coord[0], coord[1]].character = character;
-        return 0;
+        UpdateTileOutput(coord);
     }
 
-    
+
 }
 class Program
 {
     public static void Main(string[] args)
     {
-        Grid board= new Grid();
+        Grid board = new Grid();
         board.OutputGrid();
+
+        for (int i = 0; i < 15; i++)
+        {
+            int[] xy = board.GenerateRandXY();
+            board.changeTile(xy, "pebble");
+            Thread.Sleep(50);
+        }
     }
 }
